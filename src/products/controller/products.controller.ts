156957247d2d7df5f772020cc7ce4edace7135f1
Danilo Volles <danilo.volles@outlook.com>
2024-07-10 21:27:41 -0300

    Entity and Controller implementation

--- src/products/controller/products.controller.ts
+++ src/products/controller/products.controller.ts
@@ -1,34 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { ProductsService } from '../services/products.service';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
 
-@Controller('products')
+@Controller('/products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(
+    private readonly productsService: ProductsService
+  ) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @Post('/')
+  async createProduct(@Body() createProductDto: CreateProductDto) {
+    return await this.productsService.create(createProductDto);
   }
 
-  @Get()
-  findAll() {
-    return this.productsService.findAll();
+  @Get('/')
+  async findAllProducts() {
+    return await this.productsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  @Get('/:productId')
+  async findProductById(@Param('productId') productId: string) {
+    return await this.productsService.findOne(+productId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  @Put('/:productId')
+  async updateProductById(@Param('productId') productId: string, @Body() updateProductDto: UpdateProductDto) {
+    return await this.productsService.update(+productId, updateProductDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  @Patch('/:productId')
+  async inactiveProductById(@Param('productId') productId: string) {
+    return await this.productsService.remove(+productId);
   }
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,18 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('PRODUCTS')
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({unique: true})
+    name: string
+
+    @Column()
+    price: number
+
+    @Column()
+    description: string
+
+}
